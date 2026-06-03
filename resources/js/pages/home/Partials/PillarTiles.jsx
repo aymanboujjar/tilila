e7c49ec6152aab13520ec5f0ef2b330301fbe260
ayman boujjar <aymenboujjar12@gmail.com>
2026-06-03 10:59:38 +0100

    fixing the test error

--- resources/js/pages/home/Partials/PillarTiles.jsx
+++ resources/js/pages/home/Partials/PillarTiles.jsx
@@ -99,7 +99,10 @@ export default function PillarTiles() {
 
                 <div className="mt-10 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
                     {PILLARS.map((p) => {
-                        const { src, imageAlt } = getPillarVisual(p.href, slides);
+                        const { src, imageAlt } = getPillarVisual(
+                            p.href,
+                            slides,
+                        );
                         const alt = pickAlt(imageAlt, locale);
 
                         return (
